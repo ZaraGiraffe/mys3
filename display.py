3ce45bbd6de7acd81264706845087ec94ef5d84b
zahar <znaumets@gmail.com>
2024-11-13 18:37:47 +0200

    delete unnecessary file

--- display.py
+++
@@ -1,6 +0,0 @@
-from models import Timetable
-from utils import schedule_to_csv
-
-def display_schedule(timetable: Timetable, filename: str = "schedule.csv") -> None:
-    schedule_to_csv(timetable, filename)
-    print("Schedule displayed.")
